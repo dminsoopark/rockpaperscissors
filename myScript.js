7d96d1a5a19a2bd707bfbf7b2e5c5f588bf2ab73
Daniel Park <parkd2011@gmail.com>
2022-10-21 15:30:03 -0700

    commit files to github

---
+++ myScript.js
@@ -0,0 +1,78 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+
+function getComputerChoice(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const choice = arr[randomIndex];
+    return choice;
+}
+
+// const computerSelection = getComputerChoice(choices);
+// console.log(computerSelection);
+// const answer = prompt(`Rock, Paper, or Scissors?`);
+// const playerSelection = answer.toLowerCase();
+// console.log(playerSelection);
+let playerResult = 0;
+let computerResult = 0;
+
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return result = "You tie! Try again";
+        playRound();
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerResult += 1;
+        return result = "Congrats, you won!";
+    }
+    else if (playerSelection === 'rock' && computerSelection === `paper`) {
+        computerResult += 1;
+        return result = "You lose! Paper beats rock";
+        
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerResult += 1;
+        return result = "You lose! Rock beats scissors";
+        
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerResult += 1;
+        return result = "Congrats, you won!";
+        
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerResult += 1;
+        return result = "congrats, you won!";
+        
+    }
+    else {
+        computerResult += 1;
+        return result = "You lose! Scissors beats paper";
+        
+    }
+    return playerResult;
+    return computerResult;
+}
+
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(playerResult);
+// console.log(computerResult);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const answer = prompt(`Rock, Paper, or Scissors?`);
+        const playerSelection = answer.toLowerCase();
+        const computerSelection = getComputerChoice(choices);
+        playRound(playerSelection, computerSelection);
+        console.log(result);
+        console.log("Player score:" + " " + playerResult + ", " + "Computer score:" + " " + computerResult);
+    }
+    if (playerResult > computerResult) {
+        console.log("Congratulations, you won!");
+    }
+    else {
+        console.log("Sorry, you lose!");
+    }
+}
+
+game();
